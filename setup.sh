bf108f8b714716f7c36f0c27ef128187980e0632
Andy Richardson <andy.john.richardson@gmail.com>
2020-10-04 18:16:51 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,121 +1,60 @@
-#!/bin/bash
-GIT_USER_NAME="Andy Richardson"
-GIT_EMAIL="email@email.com"
+#/bin/bash
+# Note: Replace email below
 
+######################
+# Packages
+######################
+sudo apt update
+sudo apt install -y \
+  git build-essential curl python3 python3-pip
 
-# Setup git
-setupGit() {
-    echo "Configuring git"
-    git config --global user.name $GIT_USER_NAME
-    git config --global user.email $GIT_EMAIL
-    git config --global push.default current
-    git config --global pull.default current
-}
+######################
+# zsh
+######################
+sudo apt install -y zsh
+mkdir -p ~/.zsh
 
-# Setup terminator
-setupTerminator() {
-  sudo pacman -Sy --noconfirm terminator
-  cp -r home/.config/terminator ~/.config/
-}
+curl -L git.io/antigen > ~/.zsh/antigen.zsh
+cp home/.zshrc ~/.zshrc
 
-# Setup zsh
-setupZsh() {
-  echo "Installing zsh"
-  mkdir -p ~/.zsh
+######################
+# tmux
+######################
+sudo apt install -y tmux
+cp home/.tmux.conf ~/
 
-  # zsh
-  sudo pacman -Sy --noconfirm zsh
-  
-  # Antigen package manager
-  curl -L git.io/antigen > ~/.zsh/antigen.zsh
+mkdir -p ~/.tmux/plugins/tpm
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+~/.tmux/plugins/tpm/bin/install_plugins
+# chsh -s /usr/bin/tmux
 
-  # Copy config across
-  cp home/.zshrc ~/.zshrc
-}
+######################
+# neovim
+######################
+sudo apt install -y neovim
+        
+cp -r home/.config/nvim ~/.config/
+cp home/.vimrc ~/
 
-# Setup tmux
-setupTmux() {
-  sudo pacman -Sy --noconfirm tmux
-  cp home/.tmux.conf ~/
-  git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
-  ~/.tmux/plugins/tpm/bin/install_plugins
-  chsh -s /usr/bin/tmux
-}
-
-# Setup neovim
-setupNeovim() {
-  sudo pacman -Sy --noconfirm neovim python-pynvim
-
-  # Install package manager
-  curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
         https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-  
-  # Copy config
-  cp -r home/.config/nvim ~/.config/
-  cp home/.vimrc ~/
-  nvim -c PlugInstall -c q -c q
-}
+nvim -c PlugInstall -c q -c q
+pip3 install pynvim
 
-setupGnomeShell() {
-  ## Extensions
-  git clone https://github.com/tomha/gnome-shell-extension-workspace-switcher ~/.local/share/gnome-shell/extensions/workspace-switcher@tomha.github.com
-  pamac build --no-confirm gnome-shell-extension-pop-shell-git
-  
-  dconf write /org/gnome/shell/extensions/workspce-switcher/background-colour-active "'#bf3f3f00'"
-  dconf write /org/gnome/shell/extensions/workspce-switcher/background-colour-inactive "'#bf3f3f00'"
-  dconf write /org/gnome/shell/extensions/workspce-switcher/border-size-active 0
-  dconf write /org/gnome/shell/extensions/workspce-switcher/border-size-inactive 0
-  dconf write /org/gnome/shell/extensions/workspce-switcher/font-colour-active "'#ffffffff'"
-  dconf write /org/gnome/shell/extensions/workspce-switcher/font-colour-inactive "'#555753ff'"
-  dconf write /org/gnome/shell/extensions/workspce-switcher/font-colour-use-custom-active true
-  dconf write /org/gnome/shell/extensions/workspce-switcher/mode "'ALL'"
-  dconf write /org/gnome/shell/extensions/workspce-switcher/padding-horizontal 5
-  dconf write /org/gnome/shell/extensions/workspce-switcher/position "'LEFT'"
-  dconf write /org/gnome/shell/extensions/workspce-switcher/show-names false
+######################
+# git
+######################
+git config --global user.name "Andy Richardson"
+git config --global user.email "[email]"
 
-  ### Gnome Settings ###
-  # Go to workspace
-  dconf write /org/gnome/desktop/wm/keybindings/switch-to-workspace-1 "['<Super>1']"
-  dconf write /org/gnome/desktop/wm/keybindings/switch-to-workspace-2 "['<Super>2']"
-  dconf write /org/gnome/desktop/wm/keybindings/switch-to-workspace-3 "['<Super>3']"
-  dconf write /org/gnome/desktop/wm/keybindings/switch-to-workspace-4 "['<Super>4']"
-  dconf write /org/gnome/desktop/wm/keybindings/switch-to-workspace-5 "['<Super>5']"
-  dconf write /org/gnome/desktop/wm/keybindings/switch-to-workspace-6 "['<Super>6']"
-  dconf write /org/gnome/desktop/wm/keybindings/switch-to-workspace-7 "['<Super>7']"
-  dconf write /org/gnome/desktop/wm/keybindings/switch-to-workspace-8 "['<Super>8']"
+######################
+# docker
+######################
+curl -fsSL https://get.docker.com | sudo sh
+sudo usermod -aG docker andy
+pip3 install docker-compose
 
-  # Move focused window to workspace
-  dconf write /org/gnome/desktop/wm/keybindings/move-to-workspace-1 "['<Super><Shift>1']"
-  dconf write /org/gnome/desktop/wm/keybindings/move-to-workspace-2 "['<Super><Shift>2']"
-  dconf write /org/gnome/desktop/wm/keybindings/move-to-workspace-3 "['<Super><Shift>3']"
-  dconf write /org/gnome/desktop/wm/keybindings/move-to-workspace-4 "['<Super><Shift>4']"
-  dconf write /org/gnome/desktop/wm/keybindings/move-to-workspace-5 "['<Super><Shift>5']"
-  dconf write /org/gnome/desktop/wm/keybindings/move-to-workspace-6 "['<Super><Shift>6']"
-  dconf write /org/gnome/desktop/wm/keybindings/move-to-workspace-7 "['<Super><Shift>7']"
-  dconf write /org/gnome/desktop/wm/keybindings/move-to-workspace-8 "['<Super><Shift>8']"
-  
-  dconf write /org/gnome/desktop/wm/keybindings/close "['<Super>Backspace']"
-  dconf write /org/gnome/desktop/wm/keybindings/minimize "['']"
-  dconf write /org/gnome/shell/keybindings/switch-to-application-1 "['']"
-  dconf write /org/gnome/shell/keybindings/switch-to-application-2 "['']"
-  dconf write /org/gnome/shell/keybindings/switch-to-application-3 "['']"
-  dconf write /org/gnome/shell/keybindings/switch-to-application-4 "['']"  
-
-  dconf write /org/gnome/desktop/wm/keybindings/switch-to-workspace-left "['<Super><Control>Left', '<Super><Control>h']"
-  dconf write /org/gnome/desktop/wm/keybindings/switch-to-workspace-left "['<Super><Control>Right', '<Super><Control>l']"
-  dconf write /org/gnome/mutter/keybindings/toggle-tiled-left "['']"
-  dconf write /org/gnome/mutter/keybindings/toggle-tiled-right "['']"
-  dconf write /org/gnome/desktop/wm/keybindings/maximize "['']"
-  dconf write /org/gnome/desktop/wm/keybindings/unmaximize "['']"
-  dconf write /org/gnome/desktop/wm/keybindings/move-to-monitor-left "['']"
-  dconf write /org/gnome/desktop/wm/keybindings/move-to-monitor-right "['']"
-  dconf write /org/gnome/desktop/wm/preferences/focus-mode "'sloppy'"
-  dconf write /org/gnome/mutter/dynamic-workspaces false
-  dconf write /org/gnome/desktop/wm/preferences/num-workspaces 8
-}
-
-### Setup terminal
-sudo pacman -Syyu --noconfirm pamac ttf-font-awesome zsh tmux neovim ttf-joypixels
-
-### Install gnome shell (full)
-sudo pacman -Syyu --noconfirm gnome-extra gdm manjaro-gnome-assets manjaro-gdm-theme manjaro-settings-manager
+######################
+# emoji
+######################
+sudo apt install -y fonts-emojione gnome-characters
