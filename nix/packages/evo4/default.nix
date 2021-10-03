b0a36a01a147dac9680a711dc04c8e6f59d440a5
Andy Richardson <andy.john.richardson@gmail.com>
2021-10-03 12:35:29 +0100

    Finally got udev rule mounting/unmounting audio input

--- nix/packages/evo4/default.nix
+++ nix/packages/evo4/default.nix
@@ -3,7 +3,7 @@ pkgs.stdenv.mkDerivation {
   name = "evo4-udev";
   src = ./.;
   buildPhase = ''
-    make PREFIX=$out install
+    make ROOT=$out PREFIX=$out install
     substituteInPlace $out/local/bin/evo4.sh --replace pactl ${pkgs.pulseaudioFull}/bin/pactl
     mkdir -p $out/bin
     mv $out/local/bin/evo4.sh $out/bin/evo4
--- nix/system/audio.nix
+++ nix/system/audio.nix
@@ -5,22 +5,18 @@ let
     name = "20-evo4.rules";
     destination = "/lib/udev/rules.d/20-evo4.rules";
     text = ''
-      ACTION=="add", ATTRS{idVendor}=="2708", ATTRS{idProduct}=="0006" TAG+="systemd", ENV{EVO4_ALIVE}="true" ENV{SYSTEMD_USER_WANTS}+="evo4-switch@add.service"
-      ACTION=="remove", ATTRS{idVendor}=="2708", ATTRS{idProduct}=="0006" TAG+="systemd", ENV{EVO4_ALIVE}="false" ENV{SYSTEMD_USER_WANTS}+="evo4-switch@remove.service"
+      ACTION=="add",    ATTRS{idVendor}=="2708", ATTRS{idProduct}=="0006", TAG+="systemd", ENV{EVO4_ALIVE}+="true",  ENV{SYSTEMD_USER_WANTS}+="evo4-switch@default.service"
     '';
   };
   # Command to run
   evo4-script = pkgs.writeShellScriptBin "evo4" ''
-    if [[ "$1" == "add" ]]; then
+    if [[ "$1" == "start" ]]; then
       ${pkgs.pulseaudioFull}/bin/pactl load-module module-remap-source source_name=alsa_input.usb-Audient_EVO4-00.mic1 source_properties="device.description=EVO4Mic1" master=alsa_input.usb-Audient_EVO4-00.multichannel-input master_channel_map=front-left channel_map=mono remix=no
     fi
-    echo "$1" > /tmp/evo4log1
-    # echo "TESTING HERE"
-    # echo "$EVO4_ALIVE" $EVO4_ALIVE
-    # env
-    # env >> /tmp/evo4log1
-    # 
-    # echo "SCRIPT OVER" >> /tmp/evo4log
+
+    if [[ "$1" == "stop" ]]; then
+      ${pkgs.pulseaudioFull}/bin/pactl unload-module module-remap-source
+    fi
   '';
   evo4 = pkgs.symlinkJoin {
     name = "evo4";
@@ -53,11 +49,13 @@ in {
   };
 
   systemd.user.services."evo4-switch@" = {
-    description = "Some description";
-    wantedBy = [ "default.target" ];
+    description = "Evo4 device service";
+    unitConfig = { StopWhenUnneeded = true; };
     serviceConfig = {
       Type = "simple";
-      ExecStart = "${evo4}/bin/evo4 %I";
+      ExecStart = "${evo4}/bin/evo4 start";
+      ExecStop = "${evo4}/bin/evo4 stop";
+      RemainAfterExit = true;
     };
   };
   services.udev.packages = [ evo4 ];
